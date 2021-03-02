11e3c7eee01bd504e90c95b9cc0bb82d761ac7da
maldonadojudith18 <61207018+maldonadojudith18@users.noreply.github.com>
2021-03-02 09:29:15 -0600

    Add files via upload

---
+++ Primer_OO.py
@@ -0,0 +1,22 @@
+class Persona:
+    def init(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def mostrar(self, mostrar)
+        return mostrar
+
+
+class Empleado(Persona):
+    def init(self, nombre, edad, sueldo_bruto):
+        Persona.init(self, nombre, edad)
+        self.sueldo_bruto = sueldo_bruto
+
+
+def mostrar(self, mostrar)
+    return mostrar
+
+
+def Calcular_salario_neto(self, sueldo_bruto)
+
+class Cliente
